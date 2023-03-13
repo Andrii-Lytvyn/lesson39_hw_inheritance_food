55643ccafdad4ac88396c23c2649b174eb34139e
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-03-13 23:53:21 +0100

    Add solution ,clean code

---
+++ src/Drink.java
@@ -0,0 +1,58 @@
+/*Задача 1
+Создать класс Food (еда). В классе должны быть:
+
+Создать класс Drink (напиток) - наследника класса Food. В классе должны быть дополнительно:
+
+метод "открыть", который:
+для газированного напитка печатает "пшш",
+для негазированного - "скр",
+а для алкогольного сначала спрашивает возраст и издаёт звук, только если возраст больше 18.*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Drink extends Food {
+  public Drink(String foodName, String calories, boolean sparkling, boolean alcoholic) {
+    super(foodName, calories);
+    this.sparkling = sparkling;
+    this.alcoholic = alcoholic;
+  }
+
+  public static final int AGE = 18;
+  boolean sparkling;
+  boolean alcoholic;
+
+  public String open() throws IOException {
+    if (!alcoholic) {
+      if (sparkling) {
+        return "пшш";
+      } else {
+        return "скр";
+      }
+    } else {
+      return alcoholicOpen();
+    }
+  }
+
+  public String alcoholicOpen() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print("Age: ");
+    int age = Integer.parseInt(br.readLine());
+    if (age >= AGE) {
+      return "э рон дон дон";
+    } else {
+      return "Приходите позже";
+    }
+  }
+
+  public void setAlcoholic(boolean alcoholic) {
+    this.alcoholic = alcoholic;
+  }
+
+  public void setSparkling(boolean sparkling) {
+    this.sparkling = sparkling;
+  }
+
+  //чегото get на булианы не подкинуло
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.io.IOException;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     Food food = new Food("Жаба", "800");
     System.out.println(food.eatSmth());
+    System.out.println();
+
+    Drink drink = new Drink("Кола ", "100", true, false);
+    System.out.println(drink.open());
+    System.out.println();
+
+    Drink drink2 = new Drink("Kompot", "300", false, false);
+    System.out.println(drink2.open());
+    System.out.println();
+
+
+    Drink drink3 = new Drink("Beer", "300", false, true);
+    System.out.println(drink3.open());
+    System.out.println();
+
   }
 }
